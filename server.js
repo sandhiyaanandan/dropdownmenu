4c383548a9f72ce096ffdb1732271887481ccc1d
sandhiyaanandan <anandansandhiyajs@gmail.com>
2020-05-18 18:43:47 +0530

    modified server file

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+const cors = require('cors');
 const path = require('path');
 const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+
+const mail = require('./emailSenders');
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
-app.listen(port, () => {
-   console.log('Server is up!');
+
+// support json encoded bodies
+app.use(bodyParser.json());
+// support encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', process.env.PORT || 3001);
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/api', (req, res) => {});
+app.post('/api/createCustomer', (req, res) => {});
+app.post('/api/retrieveCustomer', (req, res) => {});
+app.post('/api/createCharge', (req, res) => {});
+app.post('/api/sendEmail', (req, res) => {});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
